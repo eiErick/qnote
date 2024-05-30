0d127bba551f8f2602c4c1ec27b3f7ccbdcf8f93
Érick <eierickdasilva@gmail.com>
2024-05-30 11:12:55 -0300

    Feature: adding file to run on desktop

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron/main');
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minWidth: 300,
+        minHeight: 450,
+    });
+    win.loadFile("index.html");
+    win.setMenu(null);
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
